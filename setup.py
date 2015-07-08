195c12bf1efb85fdbf9c425910b9d9e0ca7732f2
Vyacheslav Levit <dev@vlevit.org>
2015-07-08 22:31:23 +0300

    Cleanup setup.py spaces

--- setup.py
+++ setup.py
@@ -9,28 +9,28 @@ def read(name):
 setup(
     name='xatk',
     version='0.2.0',
-    packages = [],
+    packages=[],
     include_package_data=True,
     zip_safe=False,
     platforms=['linux', 'freebsd', 'openbsd', 'netbsd'],
-    scripts = [
+    scripts=[
         'xatk'
     ],
-    install_requires = [
+    install_requires=[
         "python-xlib>=0.15rc1"
     ],
-    dependency_links = [
+    dependency_links=[
         'http://downloads.sourceforge.net/project/python-xlib/python-xlib/0.15rc1/python-xlib-0.15rc1.tar.gz'
     ],
     # metadata for upload to PyPI
-    author = 'Vyacheslav Levit',
-    author_email = 'dev@vlevit.org',
-    description = 'keyboard-driven window switcher for X11',
-    long_description = read('README.rst'),
-    license = 'GPL',
-    keywords = 'X11 keyboard window',
+    author='Vyacheslav Levit',
+    author_email='dev@vlevit.org',
+    description='keyboard-driven window switcher for X11',
+    long_description=read('README.rst'),
+    license='GPL',
+    keywords='X11 keyboard window',
     url='http://github.com/vlevit/xatk/',
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: X11 Applications',
         'Intended Audience :: End Users/Desktop',
